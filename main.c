a0241c3c866c10c359ad79f3b9b8ef27bce6bb92
hzvoltex <95195958+hzvoltex69@users.noreply.github.com>
2025-09-05 11:29:40 -0400

    main.c

--- main.c
+++ main.c
@@ -29,3 +29,43 @@ void mostrar_lista_tickets(List *pacientes) {
   printf("tickets en espera: \n");
   
 }
+
+int main() {
+  char opcion;
+  List *tickets = list_create(); // puedes usar una lista para gestionar los pacientes
+
+  do {
+    mostrarMenuPrincipal();
+    printf("Ingrese su opción: ");
+    scanf(" %c", &opcion); // Nota el espacio antes de %c para consumir el
+                           // newline anterior
+
+    switch (opcion) {
+    case '1':
+      registrar_nuevo_ticket(tickets);
+      break;
+    case '2':
+      // Lógica para asignar prioridad
+      break;
+    case '3':
+      // Logica pa procesar siguiente ticket
+      break;
+    case '4':
+      mostrar_lista_ticket(tickets);
+      break;
+    case '5':
+      puts("Saliendo del sistema de gestión tickets soporte tecnico...");
+      break;
+    default:
+      puts("Opción no válida. Por favor, intente de nuevo.");
+    }
+    presioneTeclaParaContinuar();
+
+  } while (opcion != '5');
+
+  // Liberar recursos, si es necesario
+  list_clean(tickets);
+
+  return 0;
+}
+
