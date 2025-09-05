673183eab699e045bde9fb5d216f13b06af752f5
hzvoltex <95195958+hzvoltex69@users.noreply.github.com>
2025-09-04 23:21:21 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+//librerias a incluir
+#include 'list.c'
+#include 'list.h'
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Cuerpo principal del codigo
+int main(){
+
+
+
+
+  
+    return 0;
+}
