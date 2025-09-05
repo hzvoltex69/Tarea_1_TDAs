e4e1de0c2e92c25cb2208abf21b240f0697b1916
hzvoltex <95195958+hzvoltex69@users.noreply.github.com>
2025-09-05 10:58:06 -0400

    main.c

--- main.c
+++ main.c
@@ -1,16 +1,31 @@
-//librerias a incluir
-#include 'list.c'
-#include 'list.h'
+#include "list.h"
+#include <stdbool.h>
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+//===================================================================================================
+// FUNCIONES HERRAMIENTAS
+//===================================================================================================
 
-//Cuerpo principal del codigo
-int main(){
+void mostrarMenuPrincipal() {
+  puts("========================================");
+  puts("          Sistema de gestión de tickets de soporte técnico");
+  puts("========================================");
 
+  puts("1) Registrar Nuevo Ticket.");
+  puts("2) Modificar Prioridad de Tickets.");
+  puts("3) Procesar siguiente Ticket (segun de urgencia).");
+  puts("4) Mostrar lista de Tickets pendientes.");
+  puts("5) Salir.");
+}
 
+void registrar_nuevo_ticket(List *pacientes) {
+  printf("Registrar nuevo ticket\n");
 
+}
 
+void mostrar_lista_tickets(List *pacientes) {
+  printf("tickets en espera: \n");
   
-    return 0;
 }
